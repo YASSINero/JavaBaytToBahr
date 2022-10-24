ea742e7894e912f5b03e9c38ef340dbcecfe44b1
YASSINero <afriadeamassine@gmail.com>
2022-10-24 22:50:28 +0100

    -All objectives are achieved /!\ main thread skips 50~30 frames -> check Todo

--- app/src/main/java/com/example/bayttobahr/MainActivity.java
+++ app/src/main/java/com/example/bayttobahr/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.bayttobahr;
 
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,13 +14,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private TextView basicTxtV;
     private EditText edtTxtPart;
-    private LinearLayout mLl;
 
-    //private List<View> arrViews = new ArrayList<>();
+    //private ArrayList<View> arrViews = new ArrayList<>();
 
     private String _verse = "";
-    private int viewsCount = 0;
-    private boolean canReverse = false;
+
 
 
 
@@ -36,20 +32,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         edtTxtPart = findViewById(R.id.edtTxtPart);
 
         Button assignVl = findViewById(R.id.setTxtBtn);
-        Button prcsCreate = findViewById(R.id.createFieldsBtn);
+        Button prcsCreate = findViewById(R.id.SwitchLayoutBtn);
 
 
 
 
         assignVl.setOnClickListener(this);
         prcsCreate.setOnClickListener(this);
-
-
-        //Todo Fix Crash:
-        // Potent causes:
-        //  -setcontentView
-        //  -LayoutInflater
-        //  -Debug: SetOnClickListener(addBtn)
 
     }
 
@@ -65,25 +54,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } else
                     Toast.makeText(view.getContext(), "string too short", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.createFieldsBtn:
+            case R.id.SwitchLayoutBtn:
                 setContentView(R.layout.linear_lo_child);
-                mLl = (LinearLayout) findViewById(R.id.layoutLinear);
                 break;
         }
     }
 
+
     public void InflateView(View view)
     {
-        View dView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.duplct_view, mLl, false);
-        mLl.addView(dView, 0);
-        viewsCount++;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                LinearLayout mLl = findViewById(R.id.layoutLinear);
+                LinearLayout trgtLl = (LinearLayout)LinearLayout.inflate(MainActivity.this, R.layout.duplct_view, null);
+                mLl.addView(trgtLl, 0);  //index adds view in top (affects position)
+            }
+        });
+
+        //ViewGroup dView = (ViewGroup)LayoutInflater.from(getApplicationContext()).inflate(R.layout.duplct_view, mLl, false);
+        //dView.removeViews(0, dView.getChildCount()-1); //for Example
+
+
 //      mLl.setOrientation(LinearLayout.VERTICAL);
 
-        //Todo: fix added views organization
-        //Idea: change layout direction from hor to ver
-        //  when the hor limit of views is reached
-        // and change it again after one ver addition
-        // and repeat the process until some condition is met
+        //Todo: figure a way to take the excessive work off the main thread
+        //State:
+        //     -AddView requires too much work from main thread
+        //
     }
 
 
